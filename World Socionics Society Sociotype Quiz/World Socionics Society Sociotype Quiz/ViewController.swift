e57731a8db3fabcd195d7de46ef25c5980d684e8
Patrick Anderson <psa5dg@virginia.edu>
2016-10-11 22:57:29 -0400

    Welcome Screen

--- World Socionics Society Sociotype Quiz/World Socionics Society Sociotype Quiz/ViewController.swift
+++ World Socionics Society Sociotype Quiz/World Socionics Society Sociotype Quiz/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var welcome: UILabel!
+
+
+    @IBAction func viewEverything(_ sender: AnyObject) {
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
